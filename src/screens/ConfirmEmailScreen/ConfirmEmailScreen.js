130710ce2b7a62b59f0f19abbfa5a1078ec4a12b
Tony Cha <tonycha@Jennys-MacBook-Air.local>
2023-02-26 19:44:04 -0800

    first commit

---
+++ src/screens/ConfirmEmailScreen/ConfirmEmailScreen.js
@@ -0,0 +1,67 @@
+import React, {useState} from 'react'
+import { View, Text, StyleSheet, } from 'react-native'
+import CustomInput from '../../components/CustomInput'
+import CustomButton from '../../components/CustomButton'
+import { useNavigation } from '@react-navigation/native'
+
+
+const ConfirmEmailScreen = () => {
+    const navigation = useNavigation()
+    const [code, setCode] = useState('');
+
+    const onConfirmPressed = () => {
+        navigation.navigate('Home');
+
+    }
+    const onResendCodePressed = () => {
+    }
+    const onSignInPressed = () => {
+        navigation.navigate('SignIn');
+    }
+
+    return (
+        <View style={styles.root}>
+            <Text style={styles.title}>Confirm your email</Text>
+
+            <CustomInput 
+                placeholder="Enter your confirmation code" 
+                value={code} 
+                setValue={setCode} 
+            />
+
+            <CustomButton text="Confirm" onPress={onConfirmPressed} />    
+            <CustomButton text="Resend code" onPress={onResendCodePressed} type="SECONDARY"/>    
+            <CustomButton text="Back to Sign in" onPress={onSignInPressed} type="TERTIARY"/>    
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    root: {
+        alignItems: 'center',
+        padding: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#051C60',
+        margin: 10,
+    },
+    text: {
+        color: 'gray',
+        marginVertical: 10,
+    },
+    link: {
+        color: '#FDB075'
+    }
+})
+ 
+export default ConfirmEmailScreen
+
+
+
+
+
+
+
+
---
+++ src/screens/ForgotPasswordScreen/ForgotPasswordScreen.js
@@ -0,0 +1,63 @@
+import React, {useState} from 'react'
+import { View, Text, StyleSheet, } from 'react-native'
+import CustomInput from '../../components/CustomInput'
+import CustomButton from '../../components/CustomButton'
+import { useNavigation } from '@react-navigation/native'
+
+const ForgotPasswordScreen = () => {
+    const [username, setUsername] = useState('');
+
+    const navigation = useNavigation()
+
+    const onConfirmPressed = () => {
+        navigation.navigate('NewPassword')
+    }
+    const onSignInPressed = () => {
+        navigation.navigate('SignIn')
+    }
+
+    return (
+        <View style={styles.root}>
+            <Text style={styles.title}>Reset your password</Text>
+
+            <CustomInput 
+                placeholder="Username" 
+                value={username} 
+                setValue={setUsername} 
+            />
+
+            <CustomButton text="Send" onPress={onConfirmPressed} />    
+            <CustomButton text="Back to Sign in" onPress={onSignInPressed} type="TERTIARY"/>    
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    root: {
+        alignItems: 'center',
+        padding: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#051C60',
+        margin: 10,
+    },
+    text: {
+        color: 'gray',
+        marginVertical: 10,
+    },
+    link: {
+        color: '#FDB075'
+    }
+})
+ 
+export default ForgotPasswordScreen
+
+
+
+
+
+
+
+

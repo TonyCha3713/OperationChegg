045054eb6411f718d1bfa2093d4cc6d09dff7c4b
TimTimich6 <obraztsov.timo@gmail.com>
2023-03-08 00:40:40 -0800

    fixed button and centered carousel

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -23,4 +23,3 @@ const Navigation = () => {
 };
 
 export default Navigation;
-s;
--- src/navigation/tabs.js
+++ src/navigation/tabs.js
@@ -1,81 +1,61 @@
-import {View, Text, Image, StyleSheet, TouchableOpacity, Touchable, Pressable} from 'react-native'
+import { View, Text, Image, StyleSheet, TouchableOpacity, Touchable, Pressable } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import SearchTab from '../screens/SearchTab';
-import PostTab from '../screens/PostTab';
-import FolderTab from '../screens/FolderTab'
+import SearchTab from "../screens/SearchTab";
+import PostTab from "../screens/PostTab";
+import FolderTab from "../screens/FolderTab";
 import COLORS from "../colors/colors";
-import Icon from 'react-native-vector-icons/Ionicons'
+import Icon from "react-native-vector-icons/Ionicons";
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
-    return (
-        <Tab.Navigator
-            screenOptions={{
-                headerShown: false,
-                tabBarShowLabel: false,
-                tabBarStyle: {
-                    backgroundColor: COLORS.black,
-                }
-            }}>
-            <Tab.Screen 
-                name="Search" 
-                component={SearchTab} 
-                options={{
-                    tabBarIcon: ({focused}) => (
-                        <View style={{alignItems: 'center', justifyContent: 'center', top: 3}}>
-                            <Icon 
-                                name="ios-search"
-                                size={26}
-                                color={focused ? COLORS.blue : COLORS.white}
-                            />
-                            <Text style={{color: focused ? COLORS.blue : COLORS.white, fontSize: 10, fontWeight: 'bold'}}>
-                                Search
-                            </Text>
-                        </View>
-                    ),
-                }}/>
-            <Tab.Screen 
-                name="Post" 
-                component={PostTab} 
-                options={{
-                    tabBarIcon: ({focused}) => (
-                        <View style={{alignItems: 'center', justifyContent: 'center', top: 3, width: 80, height: 80}}>
-                            <Icon 
-                                name="add-circle"
-                                size={26}
-                                color={focused ? COLORS.blue : COLORS.white}
-                            />
-                            <Text style={{color: focused ? COLORS.blue : COLORS.white, fontSize: 10, fontWeight: 'bold'}}>
-                                Upload
-                            </Text>
-                        </View>
-                    ),
-                }}/>
-            <Tab.Screen 
-                name="Folder" 
-                component={FolderTab} 
-                options={{
-                    tabBarIcon: ({focused}) => (
-                        <View style={{alignItems: 'center', justifyContent: 'center', top: 3}}>
-                            <Icon 
-                                name="ios-folder"
-                                size={26}
-                                color={focused ? COLORS.blue : COLORS.white}
-                            />
-                            <Text style={{color: focused ? COLORS.blue : COLORS.white, fontSize: 10, fontWeight: 'bold'}}>
-                                Folder
-                            </Text>
-                        </View>
-                    ),
-                }}/>
-        </Tab.Navigator>
-    );
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: COLORS.black,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Search"
+        component={SearchTab}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View style={{ alignItems: "center", justifyContent: "center", top: 3 }}>
+              <Icon name="ios-search" size={26} color={focused ? COLORS.blue : COLORS.white} />
+              <Text style={{ color: focused ? COLORS.blue : COLORS.white, fontSize: 10, fontWeight: "bold" }}>Search</Text>
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Post"
+        component={PostTab}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View style={{ alignItems: "center", justifyContent: "center", top: 3, width: 80, height: 80 }}>
+              <Icon name="cloud-upload" size={26} color={focused ? COLORS.blue : COLORS.white} />
+              <Text style={{ color: focused ? COLORS.blue : COLORS.white, fontSize: 10, fontWeight: "bold" }}>Upload</Text>
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Folder"
+        component={FolderTab}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View style={{ alignItems: "center", justifyContent: "center", top: 3 }}>
+              <Icon name="ios-folder" size={26} color={focused ? COLORS.blue : COLORS.white} />
+              <Text style={{ color: focused ? COLORS.blue : COLORS.white, fontSize: 10, fontWeight: "bold" }}>Folder</Text>
+            </View>
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
 };
 
-
 export default Tabs;
-
-
-
-
-
